a569153aad6d6d405c8dcaa868e0df42cddcff97
kevinnaserwan <kevinnaserwan@gmail.com>
2024-06-18 00:19:34 +0700

    Feat: add list daftar hadir list builder

--- lib/app/daftarHadirPage.dart
+++ lib/app/daftarHadirPage.dart
@@ -1,8 +1,42 @@
-import 'package:absensi_rohis/theme/theme.dart';
+import 'package:absensi_rohis/controller/AbsenceController.dart';
 import 'package:flutter/material.dart';
+import 'package:absensi_rohis/theme/theme.dart';
 
-class DaftarHadirPage extends StatelessWidget {
-  const DaftarHadirPage({super.key});
+class DaftarHadirPage extends StatefulWidget {
+  const DaftarHadirPage({Key? key}) : super(key: key);
+
+  @override
+  _DaftarHadirPageState createState() => _DaftarHadirPageState();
+}
+
+class _DaftarHadirPageState extends State<DaftarHadirPage> {
+  AbsencesController _absencesController = AbsencesController();
+  List<Map<String, dynamic>> _absences = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchAbsences();
+  }
+
+  Future<void> fetchAbsences() async {
+    try {
+      // Replace 'your_token_here' with actual token retrieval logic
+      String token = 'your_token_here';
+
+      // Call method from AbsencesController to get all absences
+      Map<String, dynamic> response =
+          await _absencesController.getAllAbsences(token);
+
+      setState(() {
+        _absences = response['absences'].cast<Map<String, dynamic>>();
+      });
+    } catch (e) {
+      // Handle error
+      print('Error fetching absences: $e');
+      // Optionally, show error message to user
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,42 +48,58 @@ class DaftarHadirPage extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                SizedBox(height: MediaQuery.of(context).size.height * 0.02,),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.02,
+                ),
                 Column(
                   children: [
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: [Text("Daftar Hadir", style: blacktextStyle.copyWith(fontSize: 20, fontWeight: FontWeight.bold),)],
+                      children: [
+                        Text(
+                          "Daftar Hadir",
+                          style: blacktextStyle.copyWith(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ],
                     ),
                     SizedBox(
                       height: MediaQuery.of(context).size.height * 0.02,
                     ),
                     Container(
-                      decoration: BoxDecoration(color: greyColor, borderRadius: BorderRadius.circular(12)),
+                      decoration: BoxDecoration(
+                          color: greyColor,
+                          borderRadius: BorderRadius.circular(12)),
                       width: double.infinity,
                       height: MediaQuery.of(context).size.height * 0.7,
-                      child: Stack(
-                        children: [
-                          Padding(
-                            padding: EdgeInsets.all(20),
-                            child: Column(
-                              children: [
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text('Nama', style: blacktextStyle.copyWith(fontWeight: FontWeight.bold),),
-                                    Text('Status',style: blacktextStyle.copyWith(fontWeight: FontWeight.bold)),
+                      child: _absences.isNotEmpty
+                          ? ListView.builder(
+                              itemCount: _absences.length,
+                              itemBuilder: (BuildContext context, int index) {
+                                return Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 20, vertical: 10),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        _absences[index]['student']['name'],
+                                        style: blacktextStyle.copyWith(
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                      Text(
+                                          _absences[index]['status'] == 'absent'
+                                              ? 'Hadir'
+                                              : '-')
                                     ],
-                                ),
-
-                                // Tabel yang menampilkan semua nama siswa yang ada, dengan status null dan hadir
-                                Column(
-                                  children: [],
-                                )
-                              ],
-                            ),)
-                        ],
-                      ),
+                                  ),
+                                );
+                              },
+                            )
+                          : Center(
+                              child:
+                                  CircularProgressIndicator()), // Menampilkan loading indicator jika data masih belum terambil
                     ),
                   ],
                 ),
